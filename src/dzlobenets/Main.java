291477d6f08aebfafaed08f0f7292981955d28c4
noonesthere <newz1989@gmail.com>
2018-11-27 22:30:10 +0200

    git init

--- src/dzlobenets/Main.java
+++ src/dzlobenets/Main.java
@@ -1,7 +1,18 @@
 package dzlobenets;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
+        JPanel panel = new JPanel();
+        panel.setSize(500, 500);
+        panel.setLayout(new FlowLayout(FlowLayout.CENTER));
 
+        JFrame frame = new JFrame("barley-Break");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(panel);
+
+        frame.setVisible(Boolean.TRUE);
     }
 }
